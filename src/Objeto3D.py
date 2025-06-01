717f18963e2473737f3c38c0f372cbb4b118dac2
Naiumy <naiumy.reis@edu.pucrs.br>
2025-06-01 18:08:53 -0300

    mexendo na cabeça

--- src/Objeto3D.py
+++ src/Objeto3D.py
@@ -4,6 +4,7 @@ from OpenGL.GL import *
 from Ponto import *
 
 import random
+import math
 
 class Objeto3D:
         
@@ -15,39 +16,44 @@ class Objeto3D:
         self.radius   = []
         self.position = Ponto(0,0,0)
         self.rotation = (0,0,0,0)
-        pass
+        self.center   = Ponto(0,0,0)
+        self.offsets  = []
 
-    def LoadFile(self, file:str):
+    def LoadFile(self, file: str):
         f = open(file, "r")
 
         # leitor de .obj baseado na descrição em https://en.wikipedia.org/wiki/Wavefront_.obj_file    
         for line in f:
             values = line.split(' ')
-            # dividimos a linha por ' ' e usamos o primeiro elemento para saber que tipo de item temos
-
             if values[0] == 'v': 
-                # item é um vértice, os outros elementos da linha são a posição dele
-                self.vertices.append(Ponto(float(values[1]),
-                                           float(values[2]),
-                                           float(values[3])))
+                # item é um vértice
+                ponto = Ponto(float(values[1]), float(values[2]), float(values[3]))
+                self.vertices.append(ponto)
                 self.speed.append((random.random() + 0.1))
-                
-
-                self.angle.append(math.atan2(float(values[3]), float(values[1])))
-                self.radius.append(math.hypot(float(values[1]), float(values[3])))
-
+                self.angle.append(math.atan2(ponto.z, ponto.x))
+                self.radius.append(math.hypot(ponto.x, ponto.z))
 
             if values[0] == 'f':
-                # item é uma face, os outros elementos da linha são dados sobre os vértices dela
+                # item é uma face
                 self.faces.append([])
                 for fVertex in values[1:]:
                     fInfo = fVertex.split('/')
-                    # dividimos cada elemento por '/'
-                    self.faces[-1].append(int(fInfo[0]) - 1) # primeiro elemento é índice do vértice da face
-                    # ignoramos textura e normal
-                
-            # ignoramos outros tipos de items, no exercício não é necessário e vai só complicar mais
-        pass
+                    self.faces[-1].append(int(fInfo[0]) - 1)  # índice do vértice
+        f.close()
+
+        # Calcula o centro da cabeça
+        cx = sum(v.x for v in self.vertices) / len(self.vertices)
+        cy = sum(v.y for v in self.vertices) / len(self.vertices)
+        cz = sum(v.z for v in self.vertices) / len(self.vertices)
+        self.center = Ponto(cx, cy, cz)
+
+        # Calcula vetores de deslocamento (offsets) para separar as partículas
+        self.offsets = []
+        for v in self.vertices:
+            offset = v - self.center
+            comprimento = math.sqrt(offset.x**2 + offset.y**2 + offset.z**2 + 0.0001)
+            offset = offset * (1.5 / comprimento)  # normaliza e escala
+            self.offsets.append(offset)
 
     def DesenhaVertices(self):
         glPushMatrix()
@@ -56,17 +62,13 @@ class Objeto3D:
         glColor3f(.0, .0, .0)
         glPointSize(8)
 
-        # glBegin(GL_POINTS)
         for v in self.vertices:
             glPushMatrix()
             glTranslate(v.x, v.y, v.z)
             glutSolidSphere(.05, 20, 20)
             glPopMatrix()
-            # glVertex(v.x, v.y, v.z)
-        # glEnd()
         
         glPopMatrix()
-        pass
 
     def DesenhaWireframe(self):
         glPushMatrix()
@@ -83,7 +85,6 @@ class Objeto3D:
             glEnd()
         
         glPopMatrix()
-        pass
 
     def Desenha(self):
         glPushMatrix()
@@ -100,18 +101,18 @@ class Objeto3D:
             glEnd()
         
         glPopMatrix()
-        pass
 
     def ProximaPos(self, v):
         for i in range(len(self.vertices)):
-            if v: self.angle[i] += self.speed[i] * (1/30)
-            else: self.angle[i] -= self.speed[i] * (1/30)
+            if v:
+                self.angle[i] += self.speed[i] * (1/30)
+            else:
+                self.angle[i] -= self.speed[i] * (1/30)
 
             x = self.radius[i] * math.cos(self.angle[i])
             z = self.radius[i] * math.sin(self.angle[i])
 
-            self.vertices[i].x = x
-            self.vertices[i].z = z
-
-
-
+            # Aplica separação
+            self.vertices[i].x = self.center.x + x + self.offsets[i].x
+            self.vertices[i].y = self.center.y + self.offsets[i].y
+            self.vertices[i].z = self.center.z + z + self.offsets[i].z
